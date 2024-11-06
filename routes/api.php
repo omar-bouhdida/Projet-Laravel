5c4a7ca33148be5e3af81e8ce1e0553da12cd428
Omar Bouhdida <omar.bouhdida@sesame.com.tn>
2024-11-06 11:22:42 +0100

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('products', ProductController::class);
+});
+Route::middleware('auth:sanctum')->get('/user',function(Request $request){
+return $request->user();
+});
